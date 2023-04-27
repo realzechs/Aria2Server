2d2a770422fab6f99e6a964a22b05679da53ddf0
zechs <52543663+itszechs@users.noreply.github.com>
2023-04-27 18:23:47 +0530

    remove `--daemon=true`

--- startup.sh
+++ startup.sh
@@ -18,7 +18,7 @@ rclone rcd --rc-no-auth --drive-server-side-across-configs \
 
 # Aria2 RPC Server for downloading
 aria2c --enable-rpc --rpc-listen-all=true --rpc-listen-port ${PORT:-5000} \
-    --max-connection-per-server=10 --continue=true --split=10 --daemon=true \
+    --max-connection-per-server=10 --continue=true --split=10 \
     --rpc-save-upload-metadata=false --rpc-max-request-size=1024M --follow-torrent=mem \
     --allow-overwrite=true --max-concurrent-downloads=5 --seed-time=0 --bt-seed-unverified=true \
     --bt-max-peers=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 \
